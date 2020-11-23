9ee91f72914d2d85d49642404709ed4b13bb88c6
Loris Matteri <loris.matteri93@gmail.com>
2020-11-23 16:45:13 +0100

    numeri utente
    inserito prompt per chiedere di inserire un numero, fatto 5 volte con ciclo while.
    messo 5 al posto di 16 se no non si finisce più

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 
 var numeri = [];
 
-for (var i = 0; i < 16; i++) {
+for (var i = 0; i < 5; i++) {
     var randomNumber = Math.floor((Math.random() * 101));
     numeri.push(randomNumber);
 }
@@ -15,6 +15,17 @@ console.log(numeri);
 
 //In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 //L’utente non può inserire più volte lo stesso numero.
+
+var userNumbers = [];
+
+var n = 0;
+while (n < 5) {
+    var numeroUt = Number(prompt("Inserisci un numero da 1 a 100, non lo stesso più volte!"));
+    userNumbers.push(numeroUt);
+    n++;
+}
+
+console.log(userNumbers);
 
 
 
